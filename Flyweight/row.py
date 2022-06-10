50ad7a487d5272fab6049fd8a209d0bf6ef22ee0
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-06-10 18:59:33 -0500

    correct indentation

--- Flyweight/row.py
+++ Flyweight/row.py
@@ -6,14 +6,14 @@ from column import Column
 class Row:
 
     def __init__(self, column_count: int) -> None:
-        self.column = []
+        self.columns = []
         for _ in range(column_count):
             self.columns.append(Column())
 
 
-def get_data(self):
-    """Format the row before returning it to the table"""
-    ret = ""
-    for column in self.columns:
-        ret = f"{ret}{column.get_data()}|"
-    return ret
+    def get_data(self):
+        """Format the row before returning it to the table"""
+        ret = ""
+        for column in self.columns:
+            ret = f"{ret}{column.get_data()}|"
+        return ret
